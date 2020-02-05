3a31d99b9902c4c7433dd90eb6020a08734883ad
mvargas042717 <mvargas@redzonemap.com>
2020-02-04 22:24:30 -0500

    Home Page

--- mv2020/src/app/app-routing.module.ts
+++ mv2020/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {HomeComponent} from './home/home.component'
 
@@ -8,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [NgbModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
